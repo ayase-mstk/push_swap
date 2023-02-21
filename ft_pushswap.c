901db34bdd2d6d1f942504d52778a20cb3a53008
masahomeMac <masa1215.hayase@gmail.com>
2023-02-21 21:20:08 +0900

    modify

---
+++ ft_pushswap.c
@@ -0,0 +1,14 @@
+#include "push_swap.h"
+
+void	ft_pushswap(list *stack_A)
+{
+	size_t	size;
+
+	size = lst_size(stack_A);
+	if (size <= 3)
+		under_three(stack_A, size);
+	else if (size <= 6)
+		under_six(stack_A, size);
+	else
+		over_seven(stack_A, size);
+}
---
+++ utils/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <limits.h>
+# include <
+
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
+
+#endif
